736aac37b08aedbeca77f34292f675db234919fa
SoloMessiah <ch1n0ul33@gmail.com>
2021-05-13 15:58:45 -0700

    Finished structured-english.

---
+++ Final.py
@@ -0,0 +1,12 @@
+"""
+This program will display and calculate the number of grades, the average grade, and the 
+percentage of grades that are the above the average grade. We will utilize two functions.
+The first function will calculate the percentage that are above the average grade.
+The second function, the main function, will open the Final.txt file. This is where we will
+comptute the average grade and the number of grades total. We will also display them to the
+user using the following:
+
+Number of grades:
+Average grade:
+Percentage of grades above average:
+"""
