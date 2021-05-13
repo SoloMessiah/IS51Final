0a0f8a2293fd184ecc97f590551d1086bfd0e1dd
SoloMessiah <ch1n0ul33@gmail.com>
2021-05-13 16:22:39 -0700

    Finished pseudo-code.

--- Final.py
+++ Final.py
@@ -1,7 +1,7 @@
 """
 This program will display and calculate the number of grades, the average grade, and the 
 percentage of grades that are the above the average grade. We will utilize two functions.
-The first function will calculate the percentage that are above the average grade.
+The first function will calculate the percentage that are above the average grade using loop.
 The second function, the main function, will open the Final.txt file. This is where we will
 comptute the average grade and the number of grades total. We will also display them to the
 user using the following:
@@ -9,4 +9,37 @@ user using the following:
 Number of grades:
 Average grade:
 Percentage of grades above average:
+"""
+
+
+
+
+"""
+First function
+calculate_percent_above_average()
+    Set count = 0
+    Use for loop to compare score in total_grades
+        if score > average
+            increase count by 1
+    return (count * 100) / len(total_grades)
+
+Second function
+main()
+    Open Final.txt
+    Set total_grades to a list
+
+    print(Number of grades)
+
+Calculate average grade next, still within main()
+    Start average off at 0
+    for score in total_grades
+        average += score
+    average /= len(total_grades)
+
+    print(Average grade)
+    print(Percentage of grades above average)
+
+    Close Final.txt
+
+main()
 """
